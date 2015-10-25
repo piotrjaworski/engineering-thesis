988867540d4a5b7a58393d622e37bd4e644a1f94
Piotr Jaworski <piotr.jaworski@live.com>
2015-10-25 14:32:18 +0100

    Added ajax pagination in tabs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,9 +6,6 @@
 $(document).ready(function() {
   header_scroll();
   infinity_scroll_main_page("#main-pagination");
-  infinity_scroll_main_page("#user-all-pagination");
-  infinity_scroll_main_page("#user-posts-pagination");
-  infinity_scroll_main_page("#user-topics-pagination");
   clickable_link();
   topics_filter();
   category_change();
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,17 +6,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.friendly.find(params[:id])
-    # @posts = @user.latest_posts
-    # @topics = @user.latest_topics
-    # @all = (@posts + @topics).sort { |a, b| a.created_at <=> b.created_at }.paginate(page: params[:page], per_page: 10)
-    # @posts = @posts.paginate(page: params[:page], per_page: 10)
-    # @topics = @topics.paginate(page: params[:page], per_page: 10)
     @posts_count = @user.latest_posts.count
     @topics_count = @user.latest_topics.count
-    respond_to do |format|
-      format.js
-      format.html
-    end
   end
 
   def tab
