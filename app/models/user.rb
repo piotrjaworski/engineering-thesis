a45ebd4e117b1a1449ebf2b1348d0969ccd14e5c
Piotr Jaworski <piotr.jaworski@live.com>
2015-11-18 23:47:18 +0100

    small fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(response)
     data = response.info
-    user = User.find_by_email(email: data[:email])
+    user = User.find_by_email(data[:email])
     unless user
       password = Devise.friendly_token[0, 20]
       user = User.new(email: data[:email], password: password, password_confirmation: password,
